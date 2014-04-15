6377d390be1dac3a7e2db68225a76d6edd6d41fc
Shreyas <seekshreyas@gmail.com>
2014-04-15 10:40:01 -0700

    made the word remove work

--- my-app/www/js/main.js
+++ my-app/www/js/main.js
@@ -164,10 +164,16 @@ WORDCRAFT = (function(){
 	
 
 	var evtHandler = function(){
-		jQuery(document).on('tap', 'li.draggable', function(evt){
+		jQuery(document).on('vclick', 'li.draggable', function(evt){
 			console.log("draggable clicked");
-			jQuery(this).children('.circled-cross').toggle(100);
+			jQuery(this).children('.circled-cross').show(100);
+
+			jQuery(document).on('vclick', '.circled-cross', function(evt){
+				jQuery(this).parent().remove();
+			})
 		});
+
+
 	};
 
 
